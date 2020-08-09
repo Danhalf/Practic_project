5115f457d160f575712c1e90f4866ea91927a7b0
Alexey <danhalf@ukr.net>
2020-08-09 11:07:08 +0300

    add tabs

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,41 @@
+const tabs = (tabTitle, tabBody, tabContent, activeClass) => {
+  const title = document.querySelector(tabTitle),
+    tab = document.querySelectorAll(tabBody),
+    content = document.querySelectorAll(tabContent);
+
+  const hideTab = () => {
+    content.forEach((el) => {
+      el.style.display = "none";
+    });
+
+    tab.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
+  };
+
+  const showTab = (i = 0) => {
+    content[i].style.display = "block";
+    tab[i].classList.add(activeClass);
+  };
+
+  hideTab();
+  showTab();
+
+  title.addEventListener("click", (e) => {
+    const target = e.target;
+    if (
+      target &&
+      (target.classList.contains(tabBody.replace(/\./, "")) ||
+        target.parentNode.classList.contains(tabBody.replace(/\./, "")))
+    ) {
+      tab.forEach((item, index) => {
+        if (target == item || target.parentNode == item) {
+          hideTab();
+          showTab(index);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
